59669dab279a2cd9155480e0bc012aedeacd6083
Kirill Greshnov <k.greshnov@innopolis.university>
2024-12-01 17:49:34 +0300

    i give up on this bs

--- mainmain.cpp
+++ mainmain.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
-#define POP_MAX 1000
-#define MUTATION_RATE 0.1
+#define POP_MAX 200
+#define MUTATION_RATE 0.5
+#define LOW 1
+#define HIGH 99
+
+unordered_map<string, int> fitnessMap;
 
 class Soul {
 public:
@@ -48,12 +52,19 @@ public:
                 fitness_value += numbers_in_block.size();
             }
         }
+
+        fitnessMap.insert({genetics, fitness_value});
     }
 
     Soul() {}
 
     Soul(string genetics, string stencil) : genetics(genetics) {
-        calculateFitness(stencil);
+        if (fitnessMap.find(genetics) == fitnessMap.end()) {
+            calculateFitness(stencil);
+        }
+        else {
+            fitness_value = fitnessMap[genetics];
+        }
     }
 };
 
@@ -81,15 +92,14 @@ private:
         }
     }
 
-    void createPopulation() {
+    void createPopulation(int start) {
         /*
         This function generates a population based on the input data.
         Initially, every row has the correct 1-9 elements, which are
         randomly shuffled, except the given ones, those are fixed in place.
         At the end population is sorted based on fitness.
         */
-        population.resize(POP_MAX);
-        for (int k = 0; k < POP_MAX; k++) {
+        for (int k = start; k < POP_MAX; k++) {
             string soul = "";
             for (int i = 0; i < 9; i++) {
                 vector <char> perm{'1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -135,39 +145,71 @@ private:
     }
 
     void crossover(vector <Soul> &pop) {
-        vector <Soul> new_population;
+        vector <Soul> new_pop(pop.begin(), pop.begin() + POP_MAX / 2);
 
-        int ft = rand() % 80 + 1, sd = rand() % 80 + 1;
+        // random_shuffle(new_pop.begin(), new_pop.end());
 
-        exchangeIndices(ft, sd);
-
-        for (int i = 0; i < POP_MAX * 70 / 100; i += 2) {
-            string gen1 = pop[i].genetics.substr(0, ft)
-                + pop[i + 1].genetics.substr(ft, sd - ft)
-                + pop[i].genetics.substr(sd, 81);
-            string gen2 = pop[i + 1].genetics.substr(0, ft)
-                + pop[i].genetics.substr(ft, sd - ft)
-                + pop[i + 1].genetics.substr(sd, 81);
-            new_population.push_back(Soul(gen1, stencil));
-            new_population.push_back(Soul(gen2, stencil));
+        for (int i = 0; i < POP_MAX / 2; i += 2) {
+            int ft = rand() % 80 + 1, sd = rand() % 80 + 1;
+            exchangeIndices(ft, sd);
+            string gen1 = new_pop[i].genetics.substr(0, ft)
+                + new_pop[i + 1].genetics.substr(ft, sd - ft)
+                + new_pop[i].genetics.substr(sd, 81);
+            string gen2 = new_pop[i + 1].genetics.substr(0, ft)
+                + new_pop[i].genetics.substr(ft, sd - ft)
+                + new_pop[i + 1].genetics.substr(sd, 81);
+            new_pop.push_back(Soul(gen1, stencil));
+            new_pop.push_back(Soul(gen2, stencil));
         }
 
-        pop = new_population;
+        sort(
+            new_pop.begin(),
+            new_pop.end(),
+            [this](Soul soul1, Soul soul2) {
+                return this->sortBasedOnFitness(soul1, soul2);
+            }
+        );
+
+        pop = new_pop;
 
     }
 
     void mutation(vector <Soul> &pop) {
         vector <Soul> mutated_population = pop;
 
-        for (int i = 0; i < POP_MAX * 70 / 100; i++) {
-            double c = rand() % 100 / 100;
-            int id1 = rand() % 81, id2 = rand() % 81;
-            if (c <= MUTATION_RATE && stencil[id1] != '-' && stencil[id2] != '-') {
-                char temp = pop[i].genetics[id1];
-                pop[i].genetics[id1] = pop[i].genetics[id2];
-                pop[i].genetics[id2] = temp;
+        for (int itr = 0; itr < pop.size(); itr++) {
+            string gen = pop[itr].genetics;
+            for (int i = 0; i < 81; i += 9) {
+                double c = ((double) rand() / (RAND_MAX));
+                int id1 = rand() % 9, id2 = rand() % 9;
+                if (c <= MUTATION_RATE) {
+                    while (stencil[i + id1] == '-') { id1 = rand() % 9; }
+                    while (stencil[i + id2] == '-') { id2 = rand() % 9; }
+                    string gen = pop[i].genetics;
+                    char temp = gen[i + id1];
+                    gen[i + id1] = gen[i + id2];
+                    gen[i + id2] = temp;
+                }
             }
+            mutated_population[itr] = Soul(gen, stencil);
         }
+
+        // for (int itr = 0; itr < mutated_population.size(); itr++) {
+        //     for (int i = 0; i < 9; i++) {
+        //         for (int j = 0; j < 9; j++) {
+        //             double c = ((double) rand() / (RAND_MAX));
+        //             int other = rand() % 9;
+        //             if (c <= MUTATION_RATE && stencil[i * 9 + j] == '-' && other != j && stencil[i * 9 + other] == '-') {
+        //                 string gen = mutated_population[itr].genetics;
+        //                 char temp = gen[i * 9 + j];
+        //                 gen[i * 9 + j] = gen[i * 9 + other];
+        //                 gen[i * 9 + other] = temp;
+        //                 mutated_population[itr] = Soul(gen, stencil);
+        //                 continue;
+        //             }
+        //         }
+        //     }
+        // }
 
         pop = mutated_population;
 
@@ -177,18 +219,19 @@ private:
 
         vector <Soul> new_population = population;
 
-        vector <Soul> best_population(new_population.begin(), new_population.begin() + POP_MAX * 15 / 100);
-        vector <Soul> avg_population(new_population.begin() + POP_MAX * 15 / 100, new_population.begin() + POP_MAX * 85 / 100);
-        vector <Soul> worst_population(new_population.begin() + POP_MAX * 85 / 100, new_population.end());
+        crossover(new_population);
 
-        crossover(avg_population);
+        vector <Soul> best_population(new_population.begin(), new_population.begin() + POP_MAX * LOW / 100);
+        vector <Soul> avg_population(new_population.begin() + POP_MAX * LOW / 100, new_population.begin() + POP_MAX * HIGH / 100);
+        vector <Soul> worst_population(new_population.begin() + POP_MAX * HIGH / 100, new_population.end());
+
         mutation(avg_population);
 
         new_population.clear();
         new_population.insert(new_population.end(), best_population.begin(), best_population.end());
         new_population.insert(new_population.end(), avg_population.begin(), avg_population.end());
         new_population.insert(new_population.end(), worst_population.begin(), worst_population.end());
-
+ 
         sort(
             new_population.begin(),
             new_population.end(),
@@ -197,7 +240,9 @@ private:
             }
         );
 
-        if (new_population[0].fitness_value == 243) {
+        population = new_population;
+
+        if (population[0].fitness_value == 243) {
             solution = new_population[0];
             solutionFound = true;
         }
@@ -212,6 +257,7 @@ public:
     }
     
     Sudoku() {
+        population.resize(POP_MAX);
         inputStencil();
     }
 
@@ -220,23 +266,41 @@ public:
             (1) Create population
             (2) Selection
                 (2.1) Sort population based on fitness
-                (2.2) Preserve 15% best and 15% worst souls
-            (3) Crossover on other 70%
+                (2.2) Preserve some souls
+            (3) Crossover on other
             (4) Mutation on new
             (5) Run it back!
         */
 
-        createPopulation();
+        createPopulation(0);
+
+        int fit_max = 0, generation = 0;
 
         while (!solutionFound) {
             selectionEtAl();
-            cout << population[0].genetics << ' ' << population[0].fitness_value << '\n';
+
+            if (fit_max < population[0].fitness_value) {
+                cout << population[0].genetics << ' ' << population[0].fitness_value << '\n';
+                cout << population[1].genetics << ' ' << population[1].fitness_value << '\n';
+                cout << population[2].genetics << ' ' << population[2].fitness_value << "\n\n";
+                fit_max = population[0].fitness_value;
+                generation = 0;
+            }
+            else { generation++; }
+
+            if (generation >= 600) {
+                createPopulation(0);
+                fit_max = 0;
+                generation = 0;
+                cout << "\nBOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM!\n\n";
+            }
         }
     }
 };
 
 int main() {
 
+    freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
     // srand(time(0));
